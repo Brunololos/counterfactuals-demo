4ee3d4f9b33c81969ee2418b7fe9b8a7b4f85a1c
Brunololos <70522244+Brunololos@users.noreply.github.com>
2022-10-02 15:07:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
           'typeof PLUGIN_FBINSTANT': "'false'",
           'typeof FEATURE_SOUND': "'true'"
         })
-      ]
+      ],
+      base: '/counterfactuals-demo/'
     }
   }
 });
